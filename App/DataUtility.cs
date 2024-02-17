839ef09dbba659df0db22eef7345d310534e9476
Andrew Pipo <93549755+PipoAT@users.noreply.github.com>
2024-02-16 23:24:27 -0500

    Completed LINQ statements within DataUtility.cs

--- App/DataUtility.cs
+++ App/DataUtility.cs
@@ -2,7 +2,7 @@
 
 public class DataUtility
 {
-    public List<Person> People { get; set; } = [
+    public static List<Person> People { get; set; } = [
         new Person {FirstName="Brian", LastName="Dietrick", Pronoun="He",IsEmployed=true},
         new Person {FirstName="Brandi", LastName="Dietrick", Pronoun="She",IsEmployed=false},
         new Person {FirstName="Erika", LastName="Dietrick", Pronoun="She",IsEmployed=true},
@@ -24,7 +24,12 @@ public class DataUtility
         // add code below
         //**********************************************************
 
+        var dispLastNameDietrickQuery = from person in People where person.LastName == "Dietrick" select person;
 
+        foreach (var dispLastNameDietrickPerson in dispLastNameDietrickQuery)
+        {
+            Console.WriteLine(dispLastNameDietrickPerson.ToString());
+        }
     }
 
     public static async Task DisplayPeopleWithLastNameDietrickUsingApiMethodAsync()
@@ -38,8 +43,13 @@ public class DataUtility
         Console.WriteLine("**********************************************************");
         // add code below
         //**********************************************************
+        
+        var dispLastNameDietrickAPI = People.Where(person => person.LastName == "Dietrick");
 
-
+        foreach (var dispLastNameDietrickPerson in dispLastNameDietrickAPI)
+        {
+            Console.WriteLine(dispLastNameDietrickPerson.ToString());
+        }
     }
 
     public static async Task DisplayPeopleWithLastNameWithShePronounThatIsEmployedAsync()
@@ -54,7 +64,13 @@ public class DataUtility
         // add code below
         //**********************************************************
 
+        var dispDietrickSheEmployedQuery = from person in People where person.LastName == "Dietrick"
+                                            && person.Pronoun == "She" && person.IsEmployed select person;
 
+        foreach (var dispDietrickSheEmployedPerson in dispDietrickSheEmployedQuery)
+        {
+            Console.WriteLine(dispDietrickSheEmployedPerson.ToString());
+        }
     }
 
     public static async Task DisplayStudentsAsync()
@@ -69,7 +85,16 @@ public class DataUtility
         // add code below
         //**********************************************************
 
+        var dispUnemployedDietrickStudentsAPI = People.Where(person => person.LastName == "Dietrick" && !person.IsEmployed)
+                                                .Select(person => new Student {
+                                                    FirstName = person.FirstName,
+                                                    LastName = person.LastName,
+                                                    Pronoun = person.Pronoun
+                                                });
 
-
+        foreach (var dispUnemployedDietrickStudents in dispUnemployedDietrickStudentsAPI)
+        {
+            Console.WriteLine(dispUnemployedDietrickStudents.ToString());
+        }
     }
 }
